046e9d718a31d11eae10f5451481af177645bcb8
root <chaubnphan@gmail.com>
2022-09-08 18:39:07 -0700

    Remove unneccesary spaces

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,9 +4,9 @@
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0:
-            print("Fizz", end=' ')
+            print("Fizz", end='')
         elif number % 5 == 0:
-            print("Buzz", end=' ')
+            print("Buzz", end='')
         elif number % 3 and number % 5:
             print("{:d}".format(number), end='')
         print(end=' ')
