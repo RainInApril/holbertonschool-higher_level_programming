c2161f2d70d423ce61d013819895ca2e570ddf99
root <chaubnphan@gmail.com>
2022-09-08 18:02:34 -0700

    Edit z to be uppercase Z

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def uppercase(str):
     for character in str:
-        if ord('a') <= ord(character) < ord('z'):
+        if ord('a') <= ord(character) <= ord('z'):
             character = chr(ord(character) - 32)
         print("{:s}".format(character), end='')
     print("")
