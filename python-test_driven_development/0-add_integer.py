f2524345a895e9a3e372184ac4e84de61fd7526f
root <chaubnphan@gmail.com>
2022-09-25 04:05:18 -0700

    Initial commit for task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    '''Adds 2 int and returns a result.'''
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
+
