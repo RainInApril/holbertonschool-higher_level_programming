138dd0beefb6c54898243f025c47ab65338c96ec
root <chaubnphan@gmail.com>
2022-09-27 05:41:38 -0700

    Complete task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''This module contains the lookup function.'''
+
+
+def lookup(obj):
+    '''Returns the list of available attributes and methods of an objec.'''
+    return dir(obj)
