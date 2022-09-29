bcacc14b454c16ab1680d46c94c8faa492989213
root <chaubnphan@gmail.com>
2022-09-29 08:14:02 -0700

    Initial commit of task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''The BaseGeometry class.'''
+
+
+class BaseGeometry:
+    '''A class with a public instance method area.'''
+    def area(self):
+        '''Raises an exception for area.'''
+        raise Exception("area() is not implemented")
