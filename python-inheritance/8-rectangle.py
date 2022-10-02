2cce2db73587c3f8f7c2834bc78b113c618c30a0
root <chaubnphan@gmail.com>
2022-10-01 23:32:18 -0700

    Initial commit of task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''The Rectangle class.'''
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''A subclass of BaseGeometry.'''
+    def __init__(self, width, height):
+        '''Initialises a rectangle with width and height.'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
