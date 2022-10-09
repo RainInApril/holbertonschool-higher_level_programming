667661be11f50feb1dff948aa94de6e3e7ce27c1
Gem Phan <chaubnphan@gmail.com>
2022-10-09 04:10:31 -0700

    Initial commit of task 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,3 +35,12 @@ class Square(Rectangle):
         if kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, str(key), value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square."""
+        return {
+                'id': self.id,
+                'size': self.size,
+                'x': self.x,
+                'y': self.y
+        }
