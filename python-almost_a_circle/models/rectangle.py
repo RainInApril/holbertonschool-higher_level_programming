6d3f5ebdb6d0507f1d94971267113020a5272330
Gem Phan <chaubnphan@gmail.com>
2022-10-09 04:09:14 -0700

    Initial commit of task 13

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -103,3 +103,13 @@ class Rectangle(Base):
         if kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, str(key), value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle."""
+        return {
+                'x': self.x,
+                'y': self.y,
+                'id': self.id,
+                'height': self.height,
+                'width': self.width,
+        }
